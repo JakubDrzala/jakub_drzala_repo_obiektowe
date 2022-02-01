4de3c78925de8a85d030a0885e7f6f1181fb213b
lexs1305 <jakubdrzalaptn@gmail.com>
2022-02-01 19:38:31 +0100

    program milionerzy

--- 1.6 zadania/Zad3.java
+++ 1.6 zadania/Zad3.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 class Zad3 {
     public static void main(String[]args){
         Scanner sc = new Scanner (System.in);
-        System.out.printnl("Podaj imię");
-        String imie = sc.next();
+        System.out.println("Podaj imię");
+        String imie = sc.nextLine();
         System.out.println("Podaj nazwisko");
-        String nazwisko = sc.next();
+        String nazwisko = sc.nextLine();
         System.out.println("Podaj swój wiek");
+        int wiek = sc.nextInt();
     }
 }
