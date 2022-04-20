c85d73854f7a35c6fe52dc71af53857e93418c96
JakubDrzala <jakubdzrzalaptn@gmail.com>
2022-04-20 09:32:22 +0200

    tak

---
+++ metody klasy/Glowny.java
@@ -0,0 +1,10 @@
+public class Glowny {
+    public static void main(String[]args){
+        Kwadrat k1 = new Kwadrat(5);
+        Trojkat t1 = new Trojkat(10, 6);
+        System.out.println(k1.poleK());
+        System.out.println(t1.poleT());
+
+        System.out.println();
+    }
+}
---
+++ metody klasy/Kwadrat.java
@@ -0,0 +1,11 @@
+public class Kwadrat {
+    private double a;
+
+    public Kwadrat(double a){
+        this.a = a;
+    }
+
+    public double poleK(){
+        return a*a;
+    }
+}
