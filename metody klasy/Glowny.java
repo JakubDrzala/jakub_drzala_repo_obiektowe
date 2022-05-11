3b111ef74f83d881394b849b420664199d02ea05
JakubDrzala <jakubdzrzalaptn@gmail.com>
2022-05-11 09:26:42 +0200

    zadania

--- metody klasy/Glowny.java
+++ metody klasy/Glowny.java
@@ -1,10 +1,8 @@
 public class Glowny {
     public static void main(String[]args){
-        Kwadrat k1 = new Kwadrat(5);
-        Trojkat t1 = new Trojkat(10, 6);
-        System.out.println(k1.poleK());
-        System.out.println(t1.poleT());
-
-        System.out.println();
+        Liczby l = new Liczby();
+        //System.out.println(l.Kwadrat(5));
+        //System.out.println(l.Trojkat(3, 5));
+        l.Menu();
     }
 }
---
+++ metody klasy/Liczby.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Liczby {
+    public double Kwadrat(double a){
+        return a*a;
+    }
+
+    public double Trojkat(double a, double h){
+        return (a*h)/2;
+
+    }
+
+    public void  Menu(){
+        int wybor = 0;
+        System.out.println("--------");
+        System.out.println("  Menu  ");
+        System.out.println("--------");
+        System.out.println("1 - Oblicz pole kwadratu");
+        System.out.println("2 - Oblicz pole trójkąta");
+        System.out.println("3 - Historia działań");
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        switch(wybor){
+            case '1' :
+
+            break;
+
+            case '2' :
+
+            break;
+
+            case '3' :
+
+            break;
+        }
+    }
+}
